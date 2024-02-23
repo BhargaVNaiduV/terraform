d762684b459f349d0afb587c6618f9d45af4685d
bhargav <bhargavnaidu0498@gmail.com>
2024-02-23 14:04:53 -0500

    added terraform files for aws localstack

---
+++ aws/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI, replace with your desired AMI
+  instance_type = "t2.micro"
+  # key_name      = "your-key-pair-name"  # Replace with your key pair name
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+
+
+
