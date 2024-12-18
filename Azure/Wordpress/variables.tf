a75b09fb7565a800803bfd821c4bf5116047dac6
Bhargav <138513686+BhargaVNaiduV@users.noreply.github.com>
2024-12-18 18:33:51 +0000

    Added variables.tf

---
+++ Azure/Wordpress/variables.tf
@@ -0,0 +1,10 @@
+
+variable "mysql_admin_username" {
+  type        = string
+  description = "The administrator username for the MySQL server."
+}
+
+variable "mysql_admin_password" {
+  type        = string
+  description = "The administrator password for the MySQL server."
+}
